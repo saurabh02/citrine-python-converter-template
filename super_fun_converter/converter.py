3fec1796d73b439c600864b2b8e7b4f647bcede3
Max Hutchinson <maxhutch@gmail.com>
2017-03-22 09:49:51 -0500

    Adding description file and connecting it to the converter

--- super_fun_converter/converter.py
+++ super_fun_converter/converter.py
@@ -1,4 +1,14 @@
 from pypif.obj import ChemicalSystem()
 
-def convert(**kwargs):
+def convert(files=[], important_argument=None, whatever_argument=[1.0, 1.0, 1.0], **kwargs):
+    """
+    Convert files into a pif
+    :param files: to convert
+    :param important_argument: an important argument, must be provided
+    :param whatever_argument: a less important argument with default 1
+    :param kwargs: any other arguments
+    :return: the pif produced by this conversion
+    """
+    if not important_argument:
+        raise KeyError("The important_argument argument was not passed to convert")
     return ChemicalSystem() 
