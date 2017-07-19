62514ee301b4ee8e5edf8dae398526b25d29b053
Max Hutchinson <maxhutch@gmail.com>
2017-07-19 12:14:43 -0500

    Updating the description.yaml file

--- super_fun_converter/my_module.py
+++ super_fun_converter/my_module.py
@@ -1,11 +1,19 @@
 from pypif.obj import ChemicalSystem
 
-def convert(files=[], important_argument=None, whatever_argument=[1.0, 1.0, 1.0], **kwargs):
+def convert(
+        files=[],
+        important_argument=None,
+        whatever_argument=[0.0, 1.0, 2.0],
+        do_some_extra_thing=False,
+        some_condition=None,
+        **kwargs):
     """
     Convert files into a pif
+
     :param files: to convert
     :param important_argument: an important argument, must be provided
     :param whatever_argument: a less important argument with default 1
+    :param do_some_extra_thing:
     :param kwargs: any other arguments
     :return: the pif produced by this conversion
     """
